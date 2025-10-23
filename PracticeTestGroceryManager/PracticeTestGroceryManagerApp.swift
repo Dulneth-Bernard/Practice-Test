3588feb5c8ded077fa3e1a7c7c61eb865649445a
Dulneth Bernard <dulneth.bernard@gmail.com>
2025-10-23 16:30:41 +0530

    Initial Commit

---
+++ PracticeTestGroceryManager/PracticeTestGroceryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTestGroceryManagerApp.swift
+//  PracticeTestGroceryManager
+//
+//  Created by Dulneth Bernard on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTestGroceryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
