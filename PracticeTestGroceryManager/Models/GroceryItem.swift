7f5e2c61dd73c73929db6c0463f03b151245cf78
Jaindu Gajanayake <125177964+Jaindu1223@users.noreply.github.com>
2025-10-23 17:40:11 +0530

    feat: Added home view ui and updated the grocery item model by adding image

--- PracticeTestGroceryManager/Models/GroceryItem.swift
+++ PracticeTestGroceryManager/Models/GroceryItem.swift
@@ -5,5 +5,6 @@ struct GroceryItem: Identifiable {
     var id: UUID = UUID()
     var name: String
     var price: Double
+    let imageName: String
     var quantity: Int
 }
--- PracticeTestGroceryManager/Views/HomeView.swift
+++ PracticeTestGroceryManager/Views/HomeView.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var groceryItems: [GroceryItem] = [
+        GroceryItem(name: "Broccoli", price: 8.00, imageName: "broccoli", quantity: 0),
+        GroceryItem(name: "Banana", price: 10.00, imageName: "banana", quantity: 0),
+        GroceryItem(name: "Avocado", price: 7.00, imageName: "avocado", quantity: 0),
+        GroceryItem(name: "Chicken", price: 20.00, imageName: "chicken", quantity: 0),
+        GroceryItem(name: "Water", price: 5.00, imageName: "water", quantity: 0)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(header: Text("Select Grocery Items")) {
+                    ForEach($groceryItems) { $item in
+                        HStack {
+                            Image(item.imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .padding(.trailing, 10)
+                            
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(String(format: "$%.2f per item", item.price))
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            Spacer()
+                            
+            
+                            Stepper(value: $item.quantity, in: 0...10) {
+                                Text("\(item.quantity)")
+                            }
+                            .frame(width: 120)
+                        }
+                        .padding(.vertical, 5)
+                    }
+                }
+            }
+            .navigationTitle("Buy Fresh Groceries")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: CartView()) {
+                        Text("Checkout")
+                            .bold()
+                    }
+                }
+            }
+        }
     }
 }
 
